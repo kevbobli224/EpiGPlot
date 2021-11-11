338ffc4c4decb5997ca9eb51172378d900d80422
likevi23 <kevin224li@gmail.com>
2021-11-11 09:31:57 -0500

    Initial plotting, no layouts/features yet.

--- R/epiGPlotFunctions.R
+++ R/epiGPlotFunctions.R
@@ -14,19 +14,19 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 
-#' Parses epigenetic genes expression values from .csv files
+#' Parses epigenetic gene's expression values from .csv files
 #'
-#' Requires data to be in data folder, and file should contain 4 columns, separated by tabs.
+#' Requires data to be in inst/extdata folder, and file should contain 4 columns, separated by tabs.
 #'
-#' @param data A file name for the epigenetic gene expression values file
+#' @param data A file name for the epigenetic gene expression values file including the .csv suffix
 #'
-#' @return A data frame containing 4 columns of: Sample class, sample name, normalized expression value, and quantile range.
+#' @return Returns a data frame containing 4 columns of: Sample class, sample name, normalized expression value, and quantile range.
 #'
 #' @importFrom utils read.delim
 #'
 #' @export
 parseEpigeneticData <- function(data="expressions.csv"){
-    dataPath <- paste("../inst/extdata", data, sep="")
+    dataPath <- paste("../inst/extdata/", data, sep="")
     if(!file.exists(dataPath)){
         stop(sprintf("Error: data not found in: %s", dataPath))
     }
@@ -37,12 +37,74 @@ parseEpigeneticData <- function(data="expressions.csv"){
     return(dF)
 }
 
-loadEpigeneticData <- function(){
-    if(!file.exists("../data/expValues.rda")){
+#' Loads epigenetic gene's data from .rda file from the data subdirectory
+#'
+#' @param rda A string or char array containing the file name in the data subdirectory including the .rda suffix
+#'
+#' @return Returns a data frame containing 4 columns of: Sample class, sample name, normalized expression value, and quantile range.
+#'
+#' @import ggplot2
+#'
+#' @export
+loadEpigeneticData <- function(rda){
+    if(!nchar(rda)){
+        stop("Error: Empty rda file name provided!")
+    }
+    dataPath <- paste("../data/", rda, sep="")
+    if(!file.exists(dataPath)){
         stop("Error: Missing rda file for expression values!")
     }
-    tmpEnv <- new.env()
-    tmpDat <- get(load("../data/expValues.rda", envir=tmpEnv))
-    rm(tmpEnv)
+    pEnv <- new.env()
+    tmpDat <- get(load("../data/expValues.rda", envir=pEnv))
     return(tmpDat)
 }
+
+#' Plots the the expression values of a epigenetic gene.
+#'
+#' Plots the expression and quantile data in a scatterplot
+#'
+#' @param dataset An n by 4 data frame containing necessary information of epigenetic genes information, usually loaded by the package's given function; see parseEpigeneticData and loadEpigeneticData
+#'
+#' @import ggplot2
+#' @import ggforce
+plotEpigeneticEV <- function(dataset=expValues,
+                             color.color= 'black'){
+    update_geom_defaults(ggforce::GeomCircle, list(colours=color.color))
+    aesP <- ggplot2::aes(x=tmpDat[,3], y=tmpDat[,4])
+    ggplot(tmpDat[,3:4], mapping=aesP) + geom_point()
+    ggplot2::geom_point(expValues[,3:4], mapping=aes(x=expValues[,3]))
+}
+
+# head(pScaleRange(tmpDat[,3]))
+# head(tmpDat[,4])
+layoutEpigeneticEV <- function(dataFrame,
+                               normalized=FALSE){
+    if(normalized){
+        scaledEV <- pScaleRange(dataFrame[,3])
+        xLab <- "Normalized Expression Value"
+    } else {
+        scaledEV <- dataFrame[,3]
+        xLab <- "Expression Value"
+    }
+
+    layout <- cbind(labs(title="", x=xLab, y="Quantile over all Genes"))
+
+
+}
+
+legendEpigeneticEV <- function(){
+
+}
+
+# head(tmpDat[,1])
+# unique(tmpDat[,1])
+getGeneClassColour <- function(dataFrame,
+                               colourList=c("#e66101", "#fdb863", "#f7f7f7", "#b2abd2", "#5e3c99")){
+
+}
+
+pScaleRange <- function(dataFrame){
+    return((dataFrame - min(dataFrame))/(max(dataFrame)-min(dataFrame)))
+}
+
+
