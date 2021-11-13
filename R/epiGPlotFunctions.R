91e5eec58bc46645f1e47dc658cb13ce1618a826
likevi23 <kevin224li@gmail.com>
2021-11-13 12:10:52 -0500

    functions now use parameters instead of temporary placeholders

--- R/epiGPlotFunctions.R
+++ R/epiGPlotFunctions.R
@@ -26,7 +26,7 @@
 #'
 #' @export
 parseEpigeneticData <- function(data="expressions.csv"){
-    dataPath <- paste("../inst/extdata/", data, sep="")
+    dataPath <- paste("inst/extdata/", data, sep="")
     if(!file.exists(dataPath)){
         stop(sprintf("Error: data not found in: %s", dataPath))
     }
@@ -50,12 +50,12 @@ loadEpigeneticData <- function(rda){
     if(!nchar(rda)){
         stop("Error: Empty rda file name provided!")
     }
-    dataPath <- paste("../data/", rda, sep="")
+    dataPath <- paste("data/", rda, sep="")
     if(!file.exists(dataPath)){
         stop("Error: Missing rda file for expression values!")
     }
     pEnv <- new.env()
-    tmpDat <- get(load("../data/expValues.rda", envir=pEnv))
+    tmpDat <- get(load(dataPath, envir=pEnv))
     return(tmpDat)
 }
 
@@ -73,13 +73,33 @@ plotEpigeneticEV <- function(layout,
                              color.legend=TRUE,
                              color.grey=FALSE,
                              color.arrow="red",
-                             size.arrow=1,
-                             labels.num=0,
-                             labels.which=NULL){
+                             size.arrow=0.5,
+                             labels.which=NULL,
+                             sample.class=NULL,
+                             sample.pred=NULL){
 
-    #ggplot2::update_geom_defaults(ggforce::GeomCircle, list(color=layout[,3]))
-    aesP <- ggplot2::aes(x=layout[,1], y=layout[,2], color=layout[,3], label=labels.which)
+    # Validate layout parameter for plotting.
+    if(missing(layout)){
+        stop("Error: layout is missing for plotting!")
+    }
+    if(any(is.na(layout))){
+        stop("Error: layout's data contains missing values!")
+    }
+    if(!is.data.frame(layout)){
+        stop("Error: layout is not a valid data frame!")
+    }
 
+    # Validate specified sample.class parameter for plotting.
+    lLab <- c("cell_line", "fractionation", "primary_cell", "timecourse", "tissue")
+    if(!is.null(sample.class)){
+        if(length(sample.class) > 0 && sample.class %in% lLab){
+            lLab <- lLab[sample.class %in% lLab]
+        } else {
+            stop("Error: invalid specified sample.class!")
+        }
+    }
+
+    # Initialize ggplot's aesthetic
     aesP <- ggplot2::aes(x=layout[,1], y=layout[,2], color=layout[,3])
     ePlot <- ggplot2::ggplot(data=layout[,1:2]) + ggplot2::geom_point(aesP)
 
@@ -87,7 +107,7 @@ plotEpigeneticEV <- function(layout,
     yLab <- if(!normalized) "Expression Values" else "Normalized Expression Values"
 
     ePlot <- ePlot + labs(x=xLab, y=yLab)
-    lLab <- c("cell_line", "fractionation", "primary_cell", "timecourse", "tissue")
+
 
     if(!color.legend){
         ePlot <- ePlot + theme(legend.position="none")
@@ -97,15 +117,14 @@ plotEpigeneticEV <- function(layout,
     if(color.grey){
         ePlot <- ePlot + scale_color_grey(labels=lLab) + theme_classic()
     }
-    if(labels.which){
-        w <- layout[testWhich,]
+    if(!is.null(labels.which)){
         # testWhich <- c("serous cystadenocarcinoma cell line:HTOA", "chronic lymphocytic leukemia (T-CLL) cell line:SKW-3")
         # Todo: replace parameters with function parameters
-        ePlot <- ePlot + ggrepel::geom_text_repel(data=layout[testWhich,],
+        ePlot <- ePlot + ggrepel::geom_text_repel(data=layout[labels.which,],
                                                   min.segment.length = 0,
-                                                  mapping=aes(x=layout[testWhich,1],
-                                                              y=layout[testWhich,2],
-                                                              label=rownames(layout[testWhich,]),
+                                                  mapping=aes(x=layout[labels.which,1],
+                                                              y=layout[labels.which,2],
+                                                              label=rownames(layout[labels.which,]),
                                                               segment.size=1,
                                                               segment.color="red",
                                                               ),
@@ -114,8 +133,8 @@ plotEpigeneticEV <- function(layout,
                                                   arrow= arrow(length = unit(0.015, "npc")),
                                                   max.iter = 1000)
         ePlot
-
     }
+    return(ePlot)
 }
 
 # head(pScaleRange(tmpDat[,3]))
@@ -123,23 +142,33 @@ plotEpigeneticEV <- function(layout,
 # coords <- data.frame(x=scaledEV, y=tmpDat[,4])
 # colnames(coords) <- c("Expression Value","Quantile over all genes")
 # layout <- cbind(coords,labs(title="", x=xLab, y="Quantile over all Genes"))
+# sample.class <- "cell_line"
 layoutEpigeneticEV <- function(dataFrame,
-                               normalized=FALSE){
+                               normalized=FALSE,
+                               sample.class="none",
+                               predict.which="none"){
+    refDf <- dataFrame
+    sampleClass <- c("cell_line", "fractionation", "primary_cell", "timecourse", "tissue")
+    pColours <- getGeneClassColour(refDf[,1])
+    if(sample.class!="none" && sample.class %in% sampleClass){
+        refDf <- refDf[refDf[,1]==sample.class,]
+        pColours <- getGeneClassColour(refDf[,1])
+    }
+
     if(normalized){
-        scaledEV <- pScaleRange(dataFrame[,3])
+        scaledEV <- pScaleRange(refDf[,3])
         xLab <- "Normalized Expression Value"
         yDf <- scale(dataFrame[,4])
     } else {
-        scaledEV <- dataFrame[,3]
+        scaledEV <- refDf[,3]
         xLab <- "Expression Value"
-        yDf <- dataFrame[,4]
+        yDf <- refDf[,4]
     }
     layout <- data.frame(x=scaledEV, y=yDf)
-    pColours <- getGeneClassColour(dataFrame[,1])
     layout$Colours <- pColours
     # tmpSplit <- sapply(tmpDat[,2], function(x) strsplit(x,"CNhs")[[1]][1], USE.NAMES = FALSE)
 
-    rownames(layout) <- sapply(dataFrame[,2], function(x) strsplit(x,".CNhs")[[1]][1], USE.NAMES = FALSE)
+    rownames(layout) <- sapply(refDf[,2], function(x) strsplit(x,".CNhs")[[1]][1], USE.NAMES = FALSE)
     colnames(layout) <- c(xLab, "Quantile over all genes", "Colour")
     return(layout)
 }
@@ -153,6 +182,9 @@ legendEpigeneticEV <- function(){
 # layout[layout[,3]=="#e66101",3] <- "#1b9e77"
 getGeneClassColour <- function(dFClassName,
                                colourList=c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e")){
+    if(length(colourList) != 5){
+        stop("Error: custom colour list must be of length 5!")
+    }
     colours <- dFClassName
     for(gClass in unique(dFClassName)){
         switch (gClass,
@@ -171,6 +203,12 @@ pScaleRange <- function(dataFrame){
     return((dataFrame - min(dataFrame))/(max(dataFrame)-min(dataFrame)))
 }
 
+td <- loadEpigeneticData("expValues_NO66_HUMAN.rda")
+tdl <- layoutEpigeneticEV(td, sample.class="primary_cell")
+(tdp <- plotEpigeneticEV(tdl,sample.class="primary_cell"))
 
-
-
+tdm <- td[td[,1]=="primary_cell",]
+colnames(tdm) <- c("c","s","y","x")
+tlm <- lm(x~y, tdm)
+tdp + geom_abline(intercept=tlm$coefficients[1], slope=tlm$coefficients[2], colour="#7570b3", size=1)
+head(tdm)
