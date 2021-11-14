f69411507acd8088170e3e4a78e7d608e752f6c2
likevi23 <kevin224li@gmail.com>
2021-11-13 19:22:57 -0500

    Finished all package functionalities, todo: rmd and tests

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' The expression values of NO66_HUMAN Gene amongst other information of sample genes
+#'
+#' A dataset containing the expression values of sample genes as well as their
+#' respective sample class and quantile over all genes information
+#'
+#' @format A data frame with 889 rows of 4 variables:
+#' \describe{
+#'   \item{Sample Class}{Sample Class, the sample gene's respective classification}
+#'   \item{Sample}{Sample name of the gene}
+#'   \item{Expression Value}{The expression values of the associated sample gene}
+#'   \item{Quantile over all genes}{The quantile percentage over all genes}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"NO66_HUMAN"
--- R/epiGPlotFunctions.R
+++ R/epiGPlotFunctions.R
@@ -1,19 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-
 #' Parses epigenetic gene's expression values from .csv files
 #'
 #' Requires data to be in inst/extdata folder, and file should contain 4 columns, separated by tabs.
@@ -25,12 +9,15 @@
 #' @importFrom utils read.delim
 #'
 #' @export
-parseEpigeneticData <- function(data="expressions.csv"){
+parseEpigeneticData <- function(data){
+    if(!nchar(data)){
+        stop("Error: Empty rda file name provided!")
+    }
     dataPath <- paste("inst/extdata/", data, sep="")
     if(!file.exists(dataPath)){
         stop(sprintf("Error: data not found in: %s", dataPath))
     }
-    dF <- read.delim(dataPath, sep="\t")
+    dF <- utils::read.delim(dataPath, sep="\t")
     if(ncol(dF) < 4){
         stop("Error: invalid epigenetic genes expression value file format.")
     }
@@ -42,8 +29,6 @@ parseEpigeneticData <- function(data="expressions.csv"){
 #' @param rda A string or char array containing the file name in the data subdirectory including the .rda suffix
 #'
 #' @return Returns a data frame containing 4 columns of: Sample class, sample name, normalized expression value, and quantile range.
-#'
-#' @import ggplot2
 #'
 #' @export
 loadEpigeneticData <- function(rda){
@@ -61,20 +46,35 @@ loadEpigeneticData <- function(rda){
 
 #' Plots the the expression values of a epigenetic gene.
 #'
-#' Plots the expression and quantile data in a scatterplot
+#' Plots the expression vs quantile data in a scatterplot, linear regression can be visualized for user-specified sample class amongst other user-specified parameters
 #'
-#' @param dataset An n by 4 data frame containing necessary information of epigenetic genes information, usually loaded by the package's given function; see parseEpigeneticData and loadEpigeneticData
+#' @param layout An n by 4 data frame containing necessary information of epigenetic genes information, usually loaded by the package's given function; see parseEpigeneticData and loadEpigeneticData
+#' @param normalized Whether the dataset "layout" expression value is normalized.
+#' @param colour.legend Whether legend on the plot should be dispalyed
+#' @param colour.grey Whether if greyscale should be applied to all visual details of the plot
+#' @param size.arrow The size of the line segment if user chooses to display top expression values
+#' @param size.pred The size of linear regression model's line segment if user chooses to display predictions on sample class
+#' @param labels.which User can specify which sample to be displayed on the plot through a list/vector
+#' @param labels.top User can specify how many highest expression value samples to be displayed
+#' @param labels.size The font size of sample names
+#' @param sample.class User can specify which sample class to display
+#' @param sample.pred User can specify which sample class to perform linear regression on, and display their respective line segments
+#'
+#' @return Returns a plot that user can assign from its return value.
 #'
 #' @import ggplot2
 #' @importFrom ggrepel geom_text_repel
-normalized <- FALSE
+#'
+#' @export
 plotEpigeneticEV <- function(layout,
                              normalized=FALSE,
-                             color.legend=TRUE,
-                             color.grey=FALSE,
-                             color.arrow="red",
-                             size.arrow=0.5,
+                             colour.legend=TRUE,
+                             colour.grey=FALSE,
+                             size.arrow=1,
+                             size.pred=1,
                              labels.which=NULL,
+                             labels.top=3,
+                             labels.size=3,
                              sample.class=NULL,
                              sample.pred=NULL){
 
@@ -82,15 +82,21 @@ plotEpigeneticEV <- function(layout,
     if(missing(layout)){
         stop("Error: layout is missing for plotting!")
     }
+
+    # Validate if there are any NA values in given layout
     if(any(is.na(layout))){
         stop("Error: layout's data contains missing values!")
     }
+
+    # Validate if layout is a data frame
     if(!is.data.frame(layout)){
         stop("Error: layout is not a valid data frame!")
     }
 
-    # Validate specified sample.class parameter for plotting.
+    # Initialize lLab for legend labels for sample class
     lLab <- c("cell_line", "fractionation", "primary_cell", "timecourse", "tissue")
+
+    # Validate specified sample.class parameter for plotting.
     if(!is.null(sample.class)){
         if(length(sample.class) > 0 && sample.class %in% lLab){
             lLab <- lLab[sample.class %in% lLab]
@@ -98,93 +104,154 @@ plotEpigeneticEV <- function(layout,
             stop("Error: invalid specified sample.class!")
         }
     }
+    # Initialize colour matrix for sample class colour reference
+    cMat <- getColourMatrix(layout)
 
     # Initialize ggplot's aesthetic
-    aesP <- ggplot2::aes(x=layout[,1], y=layout[,2], color=layout[,3])
-    ePlot <- ggplot2::ggplot(data=layout[,1:2]) + ggplot2::geom_point(aesP)
+    aesP <- ggplot2::aes(x=layout[,1], y=layout[,2], colour=layout[,3])
 
-    xLab <- "Quantile over all genes"
-    yLab <- if(!normalized) "Expression Values" else "Normalized Expression Values"
+    # Plot using geom_point or scatterplot
+    ePlot <- ggplot2::ggplot() + ggplot2::geom_point(aesP)
 
+    # Initialize x and y axis labels
+    xLab <- if(!normalized) "Expression Values" else "Normalized Expression Values"
+    yLab <- "Quantile over all genes"
+
+    # Modify plot labels
     ePlot <- ePlot + labs(x=xLab, y=yLab)
 
-
-    if(!color.legend){
+    # User-specified legend parameters
+    if(!colour.legend){
         ePlot <- ePlot + theme(legend.position="none")
     } else {
-        ePlot <- ePlot + scale_color_manual(values=unique(layout[,3]),labels=lLab) + labs(color="Sample Class")
+        ePlot <- ePlot + scale_color_manual(values=cMat[1,],labels=lLab) + labs(colour="Sample Class")
     }
-    if(color.grey){
+    if(colour.grey){
         ePlot <- ePlot + scale_color_grey(labels=lLab) + theme_classic()
     }
-    if(!is.null(labels.which)){
-        # testWhich <- c("serous cystadenocarcinoma cell line:HTOA", "chronic lymphocytic leukemia (T-CLL) cell line:SKW-3")
-        # Todo: replace parameters with function parameters
-        ePlot <- ePlot + ggrepel::geom_text_repel(data=layout[labels.which,],
+
+    # Labelling of user-specified sample genes, if given
+    if(!is.null(labels.which) || labels.top > 0){
+        parsedLabels <- c()
+        if(!is.null(labels.which)){
+            parsedLabels <- sapply(labels.which, function(x) strsplit(x,".CNhs")[[1]][1], USE.NAMES = FALSE)
+            if(!any(parsedLabels %in% rownames(layout))){
+                stop("Error: specified label(s) does not exists in given layout data")
+            }
+            parsedLabels <- parsedLabels[parsedLabels %in% rownames(layout)]
+        }
+
+        if(labels.top > 0){
+            sortedEV <- sort(layout[,1], decreasing = TRUE)[1:labels.top]
+            parsedLabels <- c(parsedLabels, rownames(layout[which(layout[,1] %in% sortedEV),]))
+        }
+        ePlot <- ePlot + ggrepel::geom_text_repel(data=layout[parsedLabels,1:2],
                                                   min.segment.length = 0,
-                                                  mapping=aes(x=layout[labels.which,1],
-                                                              y=layout[labels.which,2],
-                                                              label=rownames(layout[labels.which,]),
-                                                              segment.size=1,
-                                                              segment.color="red",
-                                                              ),
-                                                  nudge_x = .15,
-                                                  nudge_y = .5,
-                                                  arrow= arrow(length = unit(0.015, "npc")),
-                                                  max.iter = 1000)
-        ePlot
+                                                  size=labels.size,
+                                                  segment.size=size.arrow,
+                                                  mapping=aes(x=layout[parsedLabels,1],
+                                                              y=layout[parsedLabels,2],
+                                                              label=parsedLabels,
+                                                              colour=layout[parsedLabels,3]),
+                                                  max.iter = 10000,
+                                                  box.padding = .5,
+                                                  show.legend = FALSE
+                                                  )
     }
+
+    # Performs linear regression on user-specified sample class, if given
+    if(!is.null(sample.pred)){
+        if(length(sample.pred) > 0 && any(sample.pred %in% lLab)){
+            parsedPred <- lLab[lLab %in% sample.pred]
+            oColNames <- colnames(layout)
+            colnames(layout) <- c("x","y","c","S")
+            for(pred in parsedPred){
+                modelData <- layout[which(layout[,4] %in% pred),]
+                mdl <- stats::lm(y~x,modelData)$coefficients
+                ePlot <- ePlot + ggplot2::geom_abline(intercept = mdl[1],
+                                             slope = mdl[2],
+                                             colour=cMat[1,cMat[2,] %in% pred],
+                                             size=size.pred)
+            }
+            colnames(layout) <- oColNames
+        } else {
+            stop("Error: invalid specified sample.pred!")
+        }
+    }
+
+    # Returns the plot
     return(ePlot)
 }
-
-# head(pScaleRange(tmpDat[,3]))
-# head(tmpDat[,4])
-# coords <- data.frame(x=scaledEV, y=tmpDat[,4])
-# colnames(coords) <- c("Expression Value","Quantile over all genes")
-# layout <- cbind(coords,labs(title="", x=xLab, y="Quantile over all Genes"))
-# sample.class <- "cell_line"
+#' Create a layout dataset format for plotting and better data representation
+#'
+#' Cleans up the parsed csv/rda file data from obtained dataset, and returns a layout that can be used for plotting
+#'
+#' @param dataFrame The n x 4 data frame loaded from csv/rda using the functions above
+#' @param normalized Whether or not to perform normalization on expression values
+#' @param sample.class Chooses and subset a specific sample class from the data frame and use it for layout
+#' @param class.colour User can specify a list/vector custom colour palette of length equals to number of given sample classes
+#'
+#' @return Returns a layout containing 4 columns of: expression value, quantile over all genes, colour, sample class with their row names assigned as their sample genes
+#'
+#' @importFrom grDevices col2rgb
+#'
+#' @export
 layoutEpigeneticEV <- function(dataFrame,
                                normalized=FALSE,
-                               sample.class="none",
-                               predict.which="none"){
+                               sample.class=NA,
+                               class.colour=NA){
     refDf <- dataFrame
     sampleClass <- c("cell_line", "fractionation", "primary_cell", "timecourse", "tissue")
-    pColours <- getGeneClassColour(refDf[,1])
-    if(sample.class!="none" && sample.class %in% sampleClass){
+    if(!is.na(sample.class) && any(sample.class %in% sampleClass)){
         refDf <- refDf[refDf[,1]==sample.class,]
-        pColours <- getGeneClassColour(refDf[,1])
+        if(is.na(class.colour)){
+            pColours <- getGeneClassColour(refDf[,1])
+        } else{
+            if(length(sample.class) != length(class.colour)){
+                stop("Error: mismatch class.colour size compared to sample.class")
+            }
+            if("try-error" %in% class(try(grDevices::col2rgb(class.colour), silent=TRUE))){
+                stop("Error: class.colour contains invalid colour element.")
+            }
+            pColours <- getGeneClassColour(refDf[,1], class.colour)
+        }
+    } else {
+        if(length(class.colour)!=5 && !is.na(class.colour)){
+            stop("Error: length of class.colour must be of length 5 for unspecified sample classes")
+        } else if(is.na(class.colour)){
+            pColours <- getGeneClassColour(refDf[,1])
+        } else {
+            pColours <- getGeneClassColour(refDf[,1], colourList = class.colour)
+        }
     }
 
     if(normalized){
-        scaledEV <- pScaleRange(refDf[,3])
+        scaledEV <- refDf[,3]
         xLab <- "Normalized Expression Value"
         yDf <- scale(dataFrame[,4])
     } else {
-        scaledEV <- refDf[,3]
+        scaledEV <- pScaleRange(refDf[,3])
         xLab <- "Expression Value"
         yDf <- refDf[,4]
     }
     layout <- data.frame(x=scaledEV, y=yDf)
     layout$Colours <- pColours
-    # tmpSplit <- sapply(tmpDat[,2], function(x) strsplit(x,"CNhs")[[1]][1], USE.NAMES = FALSE)
 
     rownames(layout) <- sapply(refDf[,2], function(x) strsplit(x,".CNhs")[[1]][1], USE.NAMES = FALSE)
-    colnames(layout) <- c(xLab, "Quantile over all genes", "Colour")
+    layout <- cbind(layout, refDf[,1])
+    colnames(layout) <- c(xLab, "Quantile over all genes", "Colour", "Sample Class")
     return(layout)
 }
 
-legendEpigeneticEV <- function(){
-
-}
-
-# head(tmpDat[,1])
-# unique(tmpDat[,1])
-# layout[layout[,3]=="#e66101",3] <- "#1b9e77"
+#' Obtains a colour scheme from its corresponding sample class for plotting purposes
+#'
+#' @param dFClassName A n x 1 dataframe, providing a list of sample classes
+#' @param colourList A custom colour palette for plotting purposes
+#'
+#' @return A n x 1 data frame with mapped colour string to its corresponding sample class
 getGeneClassColour <- function(dFClassName,
                                colourList=c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e")){
-    if(length(colourList) != 5){
-        stop("Error: custom colour list must be of length 5!")
-    }
+    sColourList <- sort(colourList)
     colours <- dFClassName
     for(gClass in unique(dFClassName)){
         switch (gClass,
@@ -194,21 +261,38 @@ getGeneClassColour <- function(dFClassName,
             "timecourse" = i <- 4,
             "tissue" = i <- 5
         )
-        colours[colours==gClass] <- colourList[i]
+        colours[colours==gClass] <- sColourList[i]
     }
     return(colours)
 }
 
-pScaleRange <- function(dataFrame){
-    return((dataFrame - min(dataFrame))/(max(dataFrame)-min(dataFrame)))
+#' Obtains a scale range from 0 to 1 for normalized value plotting purposes
+#'
+#' @param dFExp a single column of data frame that provides expression values of sample genes
+#'
+#' @return Returns the vectorized operated scaled values over the entire column
+pScaleRange <- function(dFExp){
+    return((dFExp - min(dFExp))/(max(dFExp)-min(dFExp)))
 }
 
-td <- loadEpigeneticData("expValues_NO66_HUMAN.rda")
-tdl <- layoutEpigeneticEV(td, sample.class="primary_cell")
-(tdp <- plotEpigeneticEV(tdl,sample.class="primary_cell"))
+#' Obtains a 2x5 colour matrix for cross-referencing colour values and sample classes
+#'
+#' @param dataFrame a layout data frame, usually used in the package's plotting function
+#'
+#' @return Returns a 2x5 matrix, where the first row contains hex colour values, and the second row contains its corresponding sample class assigned colour
+getColourMatrix <- function(dataFrame){
+    cMat <- matrix(unique(dataFrame[,3]), nrow=1, ncol=5)
+    cMat <- rbind(cMat, unique(dataFrame[,4]))
+    return(cMat)
+}
 
-tdm <- td[td[,1]=="primary_cell",]
-colnames(tdm) <- c("c","s","y","x")
-tlm <- lm(x~y, tdm)
-tdp + geom_abline(intercept=tlm$coefficients[1], slope=tlm$coefficients[2], colour="#7570b3", size=1)
-head(tdm)
+# td <- loadEpigeneticData("expValues_NO66_HUMAN.rda")
+# nc <- c("#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3")
+# tdl <- layoutEpigeneticEV(td)
+# (tdp <- plotEpigeneticEV(tdl,sample.class="primary_cell",labels.top = 7, sample.pred=tp))
+#
+# tdm <- td[td[,1]=="primary_cell",]
+# colnames(tdm) <- c("c","s","y","x")
+# tlm <- lm(x~y, tdm)
+# tdp + geom_abline(intercept=tlm$coefficients[1], slope=tlm$coefficients[2], colour="#7570b3", size=1)
+# head(tdm)
